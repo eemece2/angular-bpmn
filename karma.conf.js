31e421468801f262ee38d8528e2af7d59161d857
Eduardo Martinez Cobos <eemece2@gmail.com>
2015-12-15 21:51:01 +0100

    Remove karma conf comment

--- karma.conf.js
+++ karma.conf.js
@@ -77,7 +77,6 @@ module.exports = function(config) {
     browsers: ['Chrome'],
 
 
-    // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
   });
